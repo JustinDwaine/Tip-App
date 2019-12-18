17d183f0a74e93684e5d9225030505d4484ee5f0
Justin Armstrong <justinjustarms48@gmail.com>
2019-12-18 15:30:43 -0500

    Initial Commit

---
+++ Tip App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip App
+//
+//  Created by Justin Armstrong on 12/18/19.
+//  Copyright © 2019 Justin Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
